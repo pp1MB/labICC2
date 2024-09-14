2602d345a106a102a5763e2d19ccc736adbb4f9c
pedr-lunkes <pedrolunkesvillela@usp.br>
2024-09-14 19:05:23 -0300

    shellSort

---
+++ Aula03_Selecao/gera_caso_teste.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VALUE 1000
+#define NUM_LETTERS 10
+
+char *rand_str(int len){
+    char *s = (char*) malloc((len + 1) * sizeof(char));
+
+    for(int i = 0; i < len; i++){
+        s[i] = 'a' + rand() % 26;
+    }
+
+    s[len] = '\0';
+
+    return s;
+}
+
+int main(){
+    int caso;
+    printf("Digite o numero do caso teste: ");
+    scanf("%d", &caso);
+
+    int n;
+    printf("Digite o numero de elementos: ");
+    scanf("%d", &n);
+
+    char* nome_arquivo;
+    nome_arquivo = (char*) malloc(100 * sizeof(char));
+    sprintf(nome_arquivo, "casos_teste_nr/caso%d.in", caso);
+
+    FILE *arq = fopen(nome_arquivo, "w");
+
+    fprintf(arq, "%d\n", n);
+
+    for(int i = 0; i < n; i++){
+        char *s = rand_str(NUM_LETTERS);
+        fprintf(arq, "%s %d\n", s, rand() % MAX_VALUE);
+        free(s);
+    }
+
+    fclose(arq);
+    free(nome_arquivo);
+    return 0;
+}
---
+++ Aula03_Selecao/shellsort.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct{
+    char nome[100];
+    int pontuacao;
+} JOGADOR;
+
+
+typedef struct {
+    clock_t start;
+    clock_t end;
+} Timer;
+
+void start_timer(Timer *timer) {
+    timer->start = clock();
+}
+
+double stop_timer(Timer *timer) {
+    timer->end = clock();
+    return ((double) (timer->end - timer->start)) / CLOCKS_PER_SEC;
+}
+
+void shellSort(JOGADOR * jog, int tam){
+    int gap = 1;
+
+    while(gap < tam){
+        gap = 3*gap+1;
+    }
+
+    while(gap > 0){
+        for(int i = gap; i < tam; i++){
+            int j = i;
+            JOGADOR aux = jog[i];
+
+            while(j >= gap){
+                if(aux.pontuacao > jog[j-gap].pontuacao){
+                    jog[j] = jog[j-gap];
+                    j = j - gap;
+                }else if(aux.pontuacao == jog[j-gap].pontuacao){
+                    if(strcmp(aux.nome, jog[j-gap].nome) < 0){
+                        jog[j] = jog[j-gap];
+                        j = j - gap;
+                    }else{
+                        break;
+                    }
+                }else{
+                    break;
+                }
+            }
+
+            jog[j] = aux;
+        }
+
+        gap = gap/3;
+    }
+}
+
+int main(){
+    Timer timer;
+
+    int n;
+    scanf("%d", &n);
+
+    JOGADOR *jogadores = (JOGADOR*) malloc(n * sizeof(JOGADOR));
+
+    for(int i = 0; i < n; i++){
+        scanf("%s", jogadores[i].nome);
+        scanf("%d", &jogadores[i].pontuacao);
+    }
+
+    start_timer(&timer);
+    shellSort(jogadores, n);
+
+    double tempo = stop_timer(&timer);
+    printf("Tempo de execução: %f\n", tempo);
+
+    for(int i = 0; i < n; i++){
+        printf("Player %d: %s, Score: %d\n", i+1, jogadores[i].nome, jogadores[i].pontuacao);
+    }
+    
+    free(jogadores);
+
+    return 0;
+}

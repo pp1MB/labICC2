41769fee39a59c2c6ad0db4900a0ed7222c061ad
pp1MB <pedrohsprestes@gmail.com>
2024-10-11 13:28:47 -0300

    Juiza alterada para casos teste do runcodes

--- TimeTest/juiza.sh
+++ TimeTest/juiza.sh
@@ -1,8 +1,8 @@
 for i in {1..10}; do
     echo "$i° Caso" >> tempos.txt
     for j in {1..3}; do
-        #./a < "casoteste2/$i.in" >> bubble.txt
-        ./c < "casoteste/$i.in" >> quick.txt
+        ./merge < "casos_run/$i.in" >> tempos.txt
+        ./heap < "casos_run/$i.in" >> tempos.txt
         echo "" >> tempos.txt
     done
 done
